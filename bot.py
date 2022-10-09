07a4f32c154f42f9cd21fff20907639075099e5a
tabake <hasenberg41@mail.ru>
2022-10-09 19:00:55 +0300

    Add a help message

--- bot.py
+++ bot.py
@@ -17,6 +17,9 @@ lang_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(
 async def welcome(message: types.Message):
     await message.answer('Hello! Please select your language.\nПривет! Выберите язык.', reply_markup = lang_kb)
 
+@dp.message_handler(commands=['help'])
+async def help(message: types.Message):
+    await message.answer('This is an application for monitoring user online, Press /start to get started.\nЭто приложение для мониторинга активности пользователей. Нажмите /start для запуска.')
 
 # this is the last line
 executor.start_polling(dp)
