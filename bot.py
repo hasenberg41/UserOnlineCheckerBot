9ccf61dffe6383a3dad9b301c488ffcd7fc60ae4
tabake <hasenberg41@mail.ru>
2022-10-09 18:54:52 +0300

    Add a chose language and rename key.py

--- bot.py
+++ bot.py
@@ -1,15 +1,21 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton  # for reply keyboard (sends message)
 from time import sleep
+from bot_key import KEY
 
-import token
-
-bot = Bot(token=token.get_token())
+bot = Bot(token=KEY)
 dp = Dispatcher(bot)
 
 answers = []  # store the answers they have given
 
-### add stuff here
+lang1 = KeyboardButton('English 👍')  
+lang2 = KeyboardButton('Русский 💪')
+lang3 = KeyboardButton('Other language 🤝')
+lang_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(lang1).add(lang2).add(lang3)
+
+@dp.message_handler(commands=['start'])
+async def welcome(message: types.Message):
+    await message.answer('Hello! Please select your language.\nПривет! Выберите язык.', reply_markup = lang_kb)
 
 
 # this is the last line
